34218ac1be53bedb8a502097b1d1f12dda44e058
Tianzi Luo <tluo023@ucr.edu>
2025-08-11 14:42:27 -0700

    Add license verification

--- EccangTP_main.py
+++ EccangTP_main.py
@@ -1,4 +1,13 @@
 from ui import create_main_window
+from tkinter import messagebox
+from verify import verify_license
+import sys
+
 
 if __name__ == "__main__":
+    ok, msg = verify_license()
+    if not ok:
+        messagebox.showerror("License fail", msg)
+        sys.exit()
+
     create_main_window().mainloop()
